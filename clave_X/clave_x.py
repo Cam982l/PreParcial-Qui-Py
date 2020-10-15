044aa287480e4b14d8532dc98726192d25b85a49
Carlos Martínez <carlosm9810@gmail.com>
2020-10-14 19:53:05 -0600

    Primer commit

---
+++ clave_X/clave_x.py
@@ -0,0 +1,57 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    return result
+result = 2 + 4
+print(result)
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+    return result1
+result1 = 2 * 4 * 5
+print(result1)
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    return result2
+thislist = [2, 5, 4, 6, 9, 12]
+result2 = sum(thislist)
+print(result2)
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return ""
